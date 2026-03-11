4f0630f700e142a65c37638398d6fd54d719ed48
tracer-ics2017 <tracer@njuics.org>
2026-03-12 00:11:32 +0800

    >  compile 2413575 ics Linux 4df2092abf4e 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:11:27 up 4 days, 16:38,  0 users,  load average: 1.68, 1.02, 0.85 e025671677e2e2d29c9cd10a415f203b320098e9

--- nemu/src/device/device.c
+++ nemu/src/device/device.c
@@ -2,9 +2,13 @@
 
 #ifdef HAS_IOE
 
-#include <sys/time.h>
-#include <signal.h>
+#include <ctype.h>
+#include <fcntl.h>
 #include <SDL2/SDL.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <termios.h>
+#include <unistd.h>
 
 #define TIMER_HZ 100
 #define VGA_HZ 50
@@ -14,6 +18,10 @@ static struct itimerval it;
 static int device_update_flag = false;
 static int update_screen_flag = false;
 static uint32_t input_poll_budget = 0;
+static bool terminal_input_active = false;
+static struct termios terminal_saved_termios;
+static int terminal_saved_flags = -1;
+static int terminal_esc_state = 0;
 
 void init_serial();
 void init_timer();
@@ -24,6 +32,133 @@ extern void timer_intr();
 extern bool timer_try_raise_intr();
 extern void send_key(uint8_t, bool);
 extern void update_screen();
+
+static void restore_terminal_input(void) {
+  if (!terminal_input_active) {
+    return;
+  }
+  tcsetattr(STDIN_FILENO, TCSANOW, &terminal_saved_termios);
+  if (terminal_saved_flags >= 0) {
+    fcntl(STDIN_FILENO, F_SETFL, terminal_saved_flags);
+  }
+  terminal_input_active = false;
+}
+
+static void init_terminal_input(void) {
+  if (getenv("NEMU_TERMINAL_KEYS_ACTIVE") == NULL || !isatty(STDIN_FILENO)) {
+    return;
+  }
+
+  int ret = tcgetattr(STDIN_FILENO, &terminal_saved_termios);
+  if (ret != 0) {
+    return;
+  }
+
+  struct termios raw = terminal_saved_termios;
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN);
+  raw.c_cflag |= CS8;
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 0;
+  ret = tcsetattr(STDIN_FILENO, TCSANOW, &raw);
+  if (ret != 0) {
+    return;
+  }
+
+  terminal_saved_flags = fcntl(STDIN_FILENO, F_GETFL);
+  if (terminal_saved_flags >= 0) {
+    fcntl(STDIN_FILENO, F_SETFL, terminal_saved_flags | O_NONBLOCK);
+  }
+
+  terminal_input_active = true;
+  atexit(restore_terminal_input);
+  fprintf(stderr, "note: headless terminal keyboard mode active. Type keys in this terminal; Ctrl-C to quit.\n");
+}
+
+static int ascii_to_scancode(int ch) {
+  if (isalpha(ch)) {
+    return SDL_SCANCODE_A + tolower(ch) - 'a';
+  }
+
+  if (ch >= '1' && ch <= '9') {
+    return SDL_SCANCODE_1 + ch - '1';
+  }
+
+  switch (ch) {
+    case '0': return SDL_SCANCODE_0;
+    case ' ': return SDL_SCANCODE_SPACE;
+    case '\n':
+    case '\r': return SDL_SCANCODE_RETURN;
+    case '\t': return SDL_SCANCODE_TAB;
+    case 0x7f:
+    case '\b': return SDL_SCANCODE_BACKSPACE;
+    case '-': return SDL_SCANCODE_MINUS;
+    case '=': return SDL_SCANCODE_EQUALS;
+    case '[': return SDL_SCANCODE_LEFTBRACKET;
+    case ']': return SDL_SCANCODE_RIGHTBRACKET;
+    case '\\': return SDL_SCANCODE_BACKSLASH;
+    case ';': return SDL_SCANCODE_SEMICOLON;
+    case '\'': return SDL_SCANCODE_APOSTROPHE;
+    case ',': return SDL_SCANCODE_COMMA;
+    case '.': return SDL_SCANCODE_PERIOD;
+    case '/': return SDL_SCANCODE_SLASH;
+    case '`': return SDL_SCANCODE_GRAVE;
+    default: return SDL_SCANCODE_UNKNOWN;
+  }
+}
+
+static void inject_terminal_key(uint8_t scancode) {
+  if (scancode == SDL_SCANCODE_UNKNOWN) {
+    return;
+  }
+  send_key(scancode, true);
+  send_key(scancode, false);
+}
+
+static void poll_terminal_input(void) {
+  if (!terminal_input_active) {
+    return;
+  }
+
+  char buf[64];
+  ssize_t nread = read(STDIN_FILENO, buf, sizeof(buf));
+  if (nread <= 0) {
+    return;
+  }
+
+  for (ssize_t i = 0; i < nread; i ++) {
+    unsigned char ch = (unsigned char)buf[i];
+
+    if (terminal_esc_state == 1) {
+      if (ch == '[') {
+        terminal_esc_state = 2;
+        continue;
+      }
+      terminal_esc_state = 0;
+      inject_terminal_key(SDL_SCANCODE_ESCAPE);
+    }
+    else if (terminal_esc_state == 2) {
+      switch (ch) {
+        case 'A': inject_terminal_key(SDL_SCANCODE_UP); break;
+        case 'B': inject_terminal_key(SDL_SCANCODE_DOWN); break;
+        case 'C': inject_terminal_key(SDL_SCANCODE_RIGHT); break;
+        case 'D': inject_terminal_key(SDL_SCANCODE_LEFT); break;
+        case 'H': inject_terminal_key(SDL_SCANCODE_HOME); break;
+        case 'F': inject_terminal_key(SDL_SCANCODE_END); break;
+        default: inject_terminal_key(SDL_SCANCODE_ESCAPE); break;
+      }
+      terminal_esc_state = 0;
+      continue;
+    }
+
+    if (ch == 0x1b) {
+      terminal_esc_state = 1;
+      continue;
+    }
+
+    inject_terminal_key(ascii_to_scancode(ch));
+  }
+}
 
 static void poll_sdl_events(void) {
   SDL_Event event;
@@ -61,6 +196,7 @@ static void timer_sig_handler(int signum) {
 
 void device_update() {
   if (!device_update_flag) {
+    poll_terminal_input();
     if ((++input_poll_budget & 0x3ff) == 0) {
       poll_sdl_events();
     }
@@ -76,10 +212,12 @@ void device_update() {
     update_screen_flag = false;
   }
 
+  poll_terminal_input();
   poll_sdl_events();
 }
 
 bool device_update_in_instr() {
+  poll_terminal_input();
   poll_sdl_events();
   if (timer_try_raise_intr()) {
     return true;
@@ -102,6 +240,7 @@ void init_device() {
   init_timer();
   init_vga();
   init_i8042();
+  init_terminal_input();
 
   struct sigaction s;
   memset(&s, 0, sizeof(s));
